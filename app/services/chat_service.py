1d8c0e3455f64d3d47eb3fc45a7c8074e5b69753
徐田 <j179194301@gmail.com>
2026-07-12 20:21:25 +0800

    V2.0

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -3,7 +3,9 @@ from openai import OpenAI
 from app.core.config import settings
 from typing import Generator, Any
 from app.memory.store import conversation_messages, add_message
+from app.prompts.chat import SYSTEM_PROMPT
 # from app.services.memory import conversation_messages, add_message
+from app.tools.registry import TOOLS
 
 # 从llm.client导入client
 from app.llm.client import client
@@ -14,21 +16,29 @@ from app.llm.client import client
 
 # v3.0 流式輸出
 def chat_stream(session_id: str, message: str) -> Generator[str, Any, Any]:
-    messages = conversation_messages[session_id]
+    # messages = conversation_messages[session_id]
     # messages.append({"role": "user", "content": message})
+    history = conversation_messages[session_id]
     add_message(session_id, {"role": "user", "content": message})
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        *history
+    ]
+    
     response = client.chat.completions.create(
         model=settings.MODEL_NAME,
         messages=messages,
+        tools=TOOLS,
         stream=True
     )
     answer = ""
     for chunk in response:
         if chunk.choices[0].delta.content:
             answer += chunk.choices[0].delta.content
+            print(chunk)
             yield chunk.choices[0].delta.content
     add_message(session_id, {"role": "assistant", "content": answer})
-    print(conversation_messages[session_id])
+    print(history)
 
 # v1.0 普通輸出
 # def chat(message: str) -> str:
